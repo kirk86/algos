8114de6977097939fc3fdb0e9cd61c1d9ba97a1f
kirk86 <kirk86@walla.com>
2018-03-13 00:50:53 +0000

    minibatch sgd

--- sgd.py
+++ sgd.py
@@ -1,59 +1,123 @@
 import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(2017)
+
+# reproducibility
+hid_dim = 4
+W0 = np.random.random((3, hid_dim)) - 1
+W1 = np.random.random((hid_dim, 1)) - 1
 
 
 # create simple xor network
-def simple_network():
+def xor_network():
     X = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
     y = np.array([0, 1, 1, 0]).T.reshape(4, 1)
-    learning_rate, hid_dim = 0.5, 4
-    W0 = np.random.random((3, hid_dim)) - 1
-    W1 = np.random.random((hid_dim, 1)) - 1
-    # batch_gradient_descent(1700, X, y, W0, W1, learning_rate, hid_dim)
-    stochastic_gradient_descent(1700, X, y, W0, W1, learning_rate, hid_dim)
+    learning_rate = 0.5
+    # stochastic_gradient_descent(1700, X, y, W0, W1, learning_rate)
+    # print("=======================================================")
+    batch_gradient_descent(1700, X, y, W0, W1, learning_rate)
 
 
-def batch_gradient_descent(n_epochs, X, y, W0, W1, learning_rate, hid_dim):
+def batch_gradient_descent(n_epochs, X, y, W0, W1, learning_rate):
+    fig = plt.figure()
     for epoch in range(n_epochs):
         # forward pass
         layer1 = 1 / (1 + np.exp(-(X @ W0)))  # 4x4
         layer2 = 1 / (1 + np.exp(-(layer1 @ W1)))  # 4x1
         # error measure
-        layer2_delta = (layer2 - y) * (layer2 * (1 - layer2))  # 4x1
-        layer1_delta = (layer2_delta @ W1.T) * (layer1 * (1 - layer1))  # 4x4
+        layer2_error = (layer2 - y) * (layer2 * (1 - layer2))  # 4x1
+        layer1_error = (layer2_error @ W1.T) * (layer1 * (1 - layer1))  # 4x4
         # update weights a.k.a backpropagate
-        W1 -= (learning_rate * (layer1.T @ layer2_delta))  # 4x1
-        W0 -= (learning_rate * (X.T @ layer1_delta))  # 3x4
+        W1 -= (learning_rate * (layer1.T @ layer2_error))  # 4x1
+        W0 -= (learning_rate * (X.T @ layer1_error))  # 3x4
         print("prediction output: {}".format(layer2))
+        print("true labels {}".format(y))
+        plt.plot(np.repeat(epoch, len(W1.ravel())), W1.ravel(), 'bo')
+        plt.plot(np.repeat(epoch, len(W0.ravel())), W0.ravel(), 'r+')
+    plt.show()
 
 
-def stochastic_gradient_descent(n_epochs, X, y, W0, W1, learning_rate, hid_dim):
-    layer1 = np.zeros_like(X @ W0)
-    layer2 = np.zeros_like(layer1 @ W1)
-    layer2_delta = np.zeros((4, 1))
-    layer1_delta = np.zeros((4, 4))
+def stochastic_gradient_descent(n_epochs, X, y, W0, W1, learning_rate):
+    layer1 = np.zeros((4, 4))
+    layer2 = np.zeros((4, 1))
+    layer2_error = np.zeros((4, 1))
+    layer1_error = np.zeros((4, 4))
+    indices = np.arange(X.shape[0])
+    # fig = plt.figure()
     for epoch in range(n_epochs):
         # random shuffling of data
-        indices = np.arange(X.shape[0])
         np.random.shuffle(indices)
-        for indx in range(X.shape[0]):
+        X = X[indices]
+        y = y[indices]
+        for row in indices:
             # forward pass
-            layer1[indx] = 1 / (1 + np.exp(-(X[indices][indx, :] @ W0)))
-            layer2[indx] = 1 / (1 + np.exp(-(layer1[indx] @ W1)))
+            layer1[row] = 1 / (1 + np.exp(-(X[row, :] @ W0)))
+            layer2[row] = 1 / (1 + np.exp(-(layer1[row] @ W1)))
             # # error measure a.k.a gradient
-            layer2_delta = (layer2[indx] - y[indices]) * (layer2[indx] * (1 - layer2[indx]))
-            layer1_delta[indx] = (layer2_delta[indx].reshape(1, 1) @ W1.T) * (layer1[indx] * (1 - layer1[indx]))
+            layer2_error = (layer2[row] - y) * (layer2[row] * (1 - layer2[row]))
+            layer1_error[row] = (layer2_error[row].reshape(1, 1) @ W1.T) * (layer1[row] * (1 - layer1[row]))
             # # update weights a.k.a backpropagate
-            W1 -= (learning_rate * (layer1.T @ layer2_delta))
-            W0 -= (learning_rate * (X.T @ layer1_delta))
+            W1 -= (learning_rate * (layer1.T @ layer2_error))
+            W0 -= (learning_rate * (X.T @ layer1_error))
         print("prediction output: {}".format(layer2))
-        print("true labels {}".format(y[indices]))
+        print("true labels {}".format(y))
+        # plt.plot(np.repeat(epoch, len(W1.ravel())), W1.ravel(), 'bo')
+        # plt.plot(np.repeat(epoch, len(W0.ravel())), W0.ravel(), 'r+')
+    # plt.show()
 
 
-def minibatch_gradient_descent(n_epochs):
-    pass
+# Make a prediction with coefficients
+def predict(row, coefficients):
+    yhat = coefficients[0]
+    for i in range(len(row)-1):
+        yhat += coefficients[i + 1] * row[i]
+    return yhat
 
 
-def nesterov(n_epochs):
+# Estimate linear regression coefficients using stochastic gradient descent
+def coefficients_sgd(train, l_rate, n_epoch):
+    coef = [0.0 for i in range(len(train[0]))]
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            yhat = predict(row, coef)
+            error = yhat - row[-1]
+            sum_error += error**2
+            coef[0] = coef[0] - l_rate * error
+            for i in range(len(row)-1):
+                coef[i + 1] = coef[i + 1] - l_rate * error * row[i]
+                print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+    return coef
+
+
+# Calculate coefficients
+#dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+#l_rate = 0.001
+#n_epoch = 50
+#coef = coefficients_sgd(dataset, l_rate, n_epoch)
+#print(coef)
+# Make a prediction with coefficients
+
+
+def minibatch_gradient_descent(n_epochs, X, y, W0, W1, learning_rate):
+    for epoch in range(n_epochs):
+        for batch in [2, 4]:
+            # forward pass
+            layer1 = 1 / (1 + np.exp(-(X[:batch] @ W0[:batch])))  # 4x4
+            layer2 = 1 / (1 + np.exp(-(layer1 @ W1[:batch])))  # 4x1
+            # error measure
+            layer2_error = (layer2 - y[:batch]) * (layer2 * (1 - layer2))  # 4x1
+            layer1_error = (layer2_error @ W1[:batch].T) * (layer1 * (1 - layer1))  # 4x4
+            # update weights a.k.a backpropagate
+            W1 -= (learning_rate * (layer1.T @ layer2_error))  # 4x1
+            W0 -= (learning_rate * (X[:batch].T @ layer1_error))  # 3x4
+            print("prediction output: {}".format(layer2))
+            print("true labels {}".format(y))
+            plt.plot(np.repeat(epoch, len(W1.ravel())), W1.ravel(), 'bo')
+            plt.plot(np.repeat(epoch, len(W0.ravel())), W0.ravel(), 'r+')
+
+
+def nesterov(n_epochs, X, y, W0, W1, learning_rate):
     pass
 
 
@@ -79,3 +143,6 @@ def adamax(n_epochs):
 
 def nadam(n_epochs):
     pass
+
+
+xor_network()
